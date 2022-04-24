d2298bd4f5a00ce13cddfc5833441cb812d6978e
Patrik <84462634+propellerhead42@users.noreply.github.com>
2022-04-24 15:20:11 +0200

    timer functionality

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="block" v-if="showBlock">
+  <div class="block" v-if="showBlock" @click="stopTimer">
      <p>click me</p> 
      <p>{{ seconds }} seconds</p>
+     <p>{{ reactionTime }} reacted milisec</p>
   </div>
 </template>
 
@@ -11,32 +12,42 @@ export default {
     data() {
         return {
             showBlock: false,
-            seconds: this.mSecToSec(this.delay)
+            seconds: this.mSecToSec(this.delay),
+            timer: null,
+            reactionTime: 0
         }
     },
     mounted() {
-        console.log("mounted");
         setTimeout(() => {
             this.showBlock = true;
+            this.startTimer();
         }, this.delay);
     },
     updated() {
         console.log("updated");
     },
-    unmounted() {},
     methods: {
         mSecToSec(sec) {
             let str = Math.round(sec).toString();
             let firstChar = str[0];
             return firstChar + "," + str.substring(1,3);
+        },
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            },10);
+        },
+        stopTimer(){
+            clearInterval(this.timer);
         }
     }
 
 }
 </script>
 
-<style>
-    .block{
+<style lang="scss" scoped>
+
+    .block {
         display: flex;
         flex-direction: column; 
         justify-content: center;
@@ -45,5 +56,12 @@ export default {
         height: 15rem;
         margin-top: 2rem;
         background: red;
+
+        &:hover {
+            cursor: pointer;
+            outline: 2px solid black;
+            transition: outline 0.1ms ;
+        }
     }
+
 </style>
