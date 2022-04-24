cab82cf4cf677687c7c67239ac30054802efacc6
Patrik <84462634+propellerhead42@users.noreply.github.com>
2022-04-24 15:05:08 +0200

    display block after random delay

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="block">
-     click me 
+  <div class="block" v-if="showBlock">
+     <p>click me</p> 
+     <p>{{ seconds }} seconds</p>
   </div>
 </template>
 
 <script>
 export default {
-    props: ["delay"]
+    props: ["delay"],
+    data() {
+        return {
+            showBlock: false,
+            seconds: this.mSecToSec(this.delay)
+        }
+    },
+    mounted() {
+        console.log("mounted");
+        setTimeout(() => {
+            this.showBlock = true;
+        }, this.delay);
+    },
+    updated() {
+        console.log("updated");
+    },
+    methods: {
+        mSecToSec(sec) {
+            let str = Math.round(sec).toString();
+            let firstChar = str[0];
+            return firstChar + "," + str.substring(1,3);
+        }
+    }
+
 }
 </script>
 
 <style>
     .block{
         display: flex;
+        flex-direction: column; 
         justify-content: center;
         align-items: center;
         width: 15rem;
