2f0a19e49d7323fd149acde7be4507e3967807ab
Patrik <84462634+propellerhead42@users.noreply.github.com>
2022-04-24 13:45:10 +0200

    create start method and random delay

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start">Start</button>
+  <div class="block">
+    
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000 //returns number between 2000 and 7000
+      console.log(this.delay);
+    }
+  }
 }
 </script>
 
